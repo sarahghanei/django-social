bcf2e4756a0b68ad2cf9f137bd83c4ecb3b54d9e
amirbigg <amirbig44@gmail.com>
2019-12-31 13:56:18 +0330

    add ep6

--- account/forms.py
+++ account/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 
 
+messages = {
+	'required':'این فیلد اجباری است',
+	'invalid':'لطفا یک ایمیل معتبر وارد کنید',
+	'max_length':'تعداد کاراکترها بیشتر از حد مجاز است'
+}
+
 class UserLoginForm(forms.Form):
 	username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}))
 	password = forms.CharField(max_length=40, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
 
 
 class UserRegistrationForm(forms.Form):
-	username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-	email = forms.EmailField(max_length=50, widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}))
-	password = forms.CharField(max_length=40, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+	username = forms.CharField(error_messages=messages, max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+	email = forms.EmailField(error_messages=messages, max_length=50, widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}))
+	password = forms.CharField(error_messages=messages, max_length=40, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
