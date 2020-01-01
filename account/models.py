11a2be92369df241f2f67b96a1dc4968bc7309c5
amirbigg <amirbig44@gmail.com>
2020-01-01 13:18:17 +0330

    add ep8

--- account/models.py
+++ account/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	bio = models.TextField()
-	age = models.PositiveSmallIntegerField()
+	bio = models.TextField(null=True, blank=True)
+	age = models.PositiveSmallIntegerField(null=True, blank=True)
+
+	def __str__(self):
+		return self.user.username
+
+
+def save_profile(sender, **kwargs):
+	if kwargs['created']:
+		p1 = Profile(user=kwargs['instance'])
+		p1.save()
+
+post_save.connect(save_profile, sender=User)
