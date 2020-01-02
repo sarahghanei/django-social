e7468865012ef7837087044b031cf74715997aa8
amirbigg <amirbig44@gmail.com>
2020-01-02 12:13:18 +0330

    add ep12

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import AddPostForm
 
 
 
@@ -11,3 +12,11 @@ def all_posts(request):
 def post_detail(request, year, month, day, slug):
 	post = get_object_or_404(Post, created__year=year, created__month=month, created__day=day, slug=slug)
 	return render(request, 'posts/post_detail.html', {'post':post})
+
+
+def add_post(request, user_id):
+	if request.method == 'POST':
+		pass
+	else:
+		form = AddPostForm()
+	return render(request, 'posts/add_post.html', {'form':form})
