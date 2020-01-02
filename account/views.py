af259cab276a02d5d353355bc07fc09feed7ee44
amirbigg <amirbig44@gmail.com>
2020-01-02 11:29:37 +0330

    add ep11

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.models import User
+from posts.models import Post
 
 
 def user_login(request):
@@ -43,4 +43,25 @@ def user_logout(request):
 
 def user_dashboard(request, user_id):
 	user = get_object_or_404(User, pk=user_id)
-	return render(request, 'account/dashboard.html', {'user':user})
+	posts = Post.objects.filter(user=user)
+	return render(request, 'account/dashboard.html', {'user':user, 'posts':posts})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
