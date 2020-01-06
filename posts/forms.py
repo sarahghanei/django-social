e6f7f670308d58cf288d6acaa0be6a05377df461
amirbigg <amirbig44@gmail.com>
2020-01-06 15:23:26 +0330

    add ep23

--- posts/forms.py
+++ posts/forms.py
@@ -18,3 +18,14 @@ class AddCommentForm(forms.ModelForm):
 	class Meta:
 		model = Comment
 		fields = ('body',)
+		widgets = {
+			'body': forms.Textarea(attrs={'class':'form-control'})
+		}
+		error_messages = {
+			'body':{
+				'required': 'این فیلد اجباری است',
+			}
+		}
+		help_texts = {
+			'body':'max 400 char'
+		}
